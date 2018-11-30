f17143c6954c2f1e96ecd125a6bc330615fe5738
Daniel Dombrowsky <ddombrow@gmail.com>
2018-11-30 06:19:47 -0500

    Start

--- theme.js
+++ theme.js
@@ -8,5 +8,10 @@ export default {
   // Read the docs for more info:
   // https://github.com/jxnblk/mdx-deck/blob/master/docs/theming.md
   // https://github.com/jxnblk/mdx-deck/blob/master/docs/themes.md
-
+  font: 'Geomanist, sans-serif',
+  colors: {
+    text: '#B5226E',
+    background: '#F8F8F9',
+    link: '#273CB4',
+  }
 }
